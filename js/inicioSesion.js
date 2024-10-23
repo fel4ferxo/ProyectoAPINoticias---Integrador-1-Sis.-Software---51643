0d9847b9e51fe82eed95a6e4a09a9da884e0bd9c
KenyiPlatanazo <riositalo2@gmail.com>
2024-10-22 23:44:46 -0500

    deleted:    css/login.css modified:   css/noticia.css modified:   index.html modified:   js/Registro.js modified:   js/inicioSesion.js modified:   js/news.js deleted:    login.html deleted:    views/index.html modified:   views/inicio-sesion.html modified:   views/news.html deleted:    views/noticia.html modified:   views/registro.html

--- js/inicioSesion.js
+++ js/inicioSesion.js
@@ -4,10 +4,12 @@ document.addEventListener("DOMContentLoaded", function(){
         
         const correo = document.getElementById('correo');
         const contraseña = document.getElementById('contraseña'); 
+        const errorMensaje = document.getElementById('mensaje-error');
         //Se mantiene temporalmente el ejemplo de verificación de inicio de sesión
         if (correo === 'test@example.com' && contraseña === '1234') {
             localStorage.setItem('authenticated', 'true');
         }
+
     });
 
     //Validador de campos de bootstrap
@@ -17,9 +19,20 @@ document.addEventListener("DOMContentLoaded", function(){
         Array.prototype.slice.call(forms)
         .forEach(function (form) {
             form.addEventListener('submit', function (event) {
+            var correo = document.getElementById('correo').value;
+            var contraseña = document.getElementById('contraseña').value; 
+            var errorMensaje = document.getElementById('mensaje-error');
+
             if (!form.checkValidity()) {
                 event.preventDefault()
                 event.stopPropagation()
+            }
+            else{
+                if(correo === "test@example.com" && contraseña === "1234"){
+                    window.location.href = "news.html";
+                }else{
+                    errorMensaje.classList.remove('d-none');
+                }
             }
             form.classList.add('was-validated')
             }, false)
