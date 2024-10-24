32673e145a754a67e0fb31c0d562357aee2bf9e6
alfredo124 <u19300472@utp.edu.pe>
2024-10-23 20:31:49 -0500

    actualisacion base de fronety baken

---
+++ APINoticias-Backend/src/main/java/com/ApiNoticias/APINoticias_Backend/ApiNoticiasBackendApplication.java
@@ -0,0 +1,13 @@
+package com.ApiNoticias.APINoticias_Backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiNoticiasBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiNoticiasBackendApplication.class, args);
+	}
+
+}
