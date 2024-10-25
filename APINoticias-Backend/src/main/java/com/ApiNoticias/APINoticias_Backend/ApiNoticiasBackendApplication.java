0a019388aea5f8b048d36b0e1cd90d9ceeef27d2
alfredo124 <u19300472@utp.edu.pe>
2024-10-25 00:54:21 -0500

    comprobacion
    Revert "Revert "Alfredo aportes""

--- APINoticias-Backend/src/main/java/com/ApiNoticias/APINoticias_Backend/ApiNoticiasBackendApplication.java
+++ APINoticias-Backend/src/main/java/com/ApiNoticias/APINoticias_Backend/ApiNoticiasBackendApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ApiNoticiasBackendApplication {
 
 	public static void main(String[] args) {
+		System.out.println("hola mundo");
 		SpringApplication.run(ApiNoticiasBackendApplication.class, args);
 	}
 
